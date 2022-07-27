1acf3fc89b2054bd8d5ecf7fe11c5d240fb09796
ilanebohan <91267260+ilanebohan@users.noreply.github.com>
2022-07-27 11:59:16 +0200

    Last Commit
    - Added Abstraction, Encapsulation, Inheritance and Polymorphism,
    - Beggin to work with Profiler and Optimization.

---
+++ UnityProject/Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class ProductivityUnit : Unit // replace MonoBehaviour with Unit.
+{
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
+    // POLYMORPHISM - OVERRIDE
+    protected override void BuildingInRange()
+    {
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+
+    }
+
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+
+    // POLYMORPHISM - OVERRIDE
+    public override void GoTo(Building target)
+    {
+        ResetProductivity(); // call your new method
+        base.GoTo(target); // run method from base class
+    }
+
+    // POLYMORPHISM - OVERRIDE
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+}
